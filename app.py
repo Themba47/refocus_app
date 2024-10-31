ef7ee0a02c7f15f8a8efd7935458b1429648c449
Themba47 <sishubats@gmail.com>
2024-10-31 20:01:51 +0200

    oops

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def home():
    return render_template('index.html', questions=questions)
 
  
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8885', debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port='8885', debug=True)
